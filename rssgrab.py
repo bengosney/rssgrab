04b2906d20e751622baa4d901962d6e918c35d9b
Ben Gosney <ben@mediaserver.(none)>
2011-08-18 22:08:27 +0100

    Fixed an isue with config path when running from a crontab, not sure how I missed this before

--- rssgrab.py
+++ rssgrab.py
@@ -19,11 +19,12 @@
 import urllib
 import os
 import ConfigParser
-from xml.dom.minidom import parse
+from xml.dom.minidom import *
 
 # read all the settings from the ini file
 Config = ConfigParser.ConfigParser()
-Config.read("config.ini")
+ConfigPath = os.path.dirname(os.path.abspath(__file__)) + "/config.ini"
+Config.read(ConfigPath)
 
 rsslist = Config.get("Main", "rsslist")
 savepath = Config.get("Main", "savepath")
@@ -31,40 +32,57 @@ savepath = Config.get("Main", "savepath")
 flist = file(rsslist)                       # load the rss feed list
 
 for rssline in flist:                       # step through the list
-    rssline = rssline.rstrip().split(',')   # slip the name and the url
+    if rssline[0] == '#':                   # allow for comments in the rss list
+        continue
+
+    rssline = rssline.rstrip().split(',')   # split the name and the url
     
-    if not os.path.exists(savepath + rssline[0]):      # make the sub directory if it doesn't exist
+    if not os.path.exists(savepath + rssline[0]):      # make the sub directory
+        # if it doesn't exist
         os.makedirs(savepath + rssline[0])
     
     print "Checking " + rssline[0]
 
-    dom = parse(rssline[1])    # get the rss feed
+    dom = parse(urllib.urlopen(rssline[1])) # download the rssfeed
+
+    listoffile = ""                         # reset list of files
     
-    listoffile = ""                                 # reset list of files
-    
-    for node in dom.getElementsByTagName('enclosure'):                                 # step through the rss feed
-        fn = node.getAttribute('url')
-        fullpath = savepath + rssline[0] + "/" + os.path.basename(fn)   # make the full path for easy of reading
+    dlcount = 0    # reset download count for this podcast
+
+    for node in dom.getElementsByTagName('item'):                                 # step through the rss feed
+        pTitle = node.getElementsByTagName('title').item(0).childNodes.item(0).nodeValue    # get title, in a really nasty looking way
+        pUrl = node.getElementsByTagName('enclosure').item(0).getAttribute('url')   # get the url
+
+        pName = pTitle + os.path.splitext(pUrl)[1]    # Make file name
+        fullpath = savepath + rssline[0] + "/" + pName   # make the full path for easy of reading
         
-        listoffile = listoffile + "," + os.path.basename(fn)    # build a string of the file names to check deleting old podcasts
+        listoffile = listoffile + "," + pName    # build a string of the file names to check deleting old podcasts
         
         if not os.path.exists(fullpath): # make sure we don't already have the file
-            print "Downloading " + fn
+            print "Downloading " + pName
             
-            podcast = urllib.urlopen(fn)        # get the video file
+            podcast = urllib.urlopen(pUrl)        # get the video file
             
             fout = file(fullpath,'w')           # open the output file
             fout.write(podcast.read())          # write the file
             fout.close()
     
             print "Downloaded"      
+
+        # Limit the downloaded files
+        dlcount = dlcount + 1    # incrament the downloaded count
+        print "Downloaded: " + str(dlcount) + " of " + str(rssline[2])    # Print the count
+        if dlcount == int(rssline[2]):    # Break if you've reached the download the limit
+            break
+
+
     print "Cleaning up old podcasts"
     
     for root, dir, files in os.walk(savepath + rssline[0] + "/"):   # walk the podcast directory
         for tmp in files:
-            if (listoffile.find(tmp) < 0):                          # make sure the podcast is still in the feed
+            if (listoffile.find(tmp) < 0):                          # check to see if we want the podcast or not
                 print "removing " + tmp
-                os.remove(savepath + rssline[0] + "/" + tmp)        #delete file
+                os.remove(savepath + rssline[0] + "/" + tmp)        # delete file if we don't want it
                                                    
     print rssline[0] + " is upto date" # close up and "stuff"
         
